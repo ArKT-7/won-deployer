0c487c294893b76d496a456ea7785f944b509889
ArKT <157277855+ArKT-7@users.noreply.github.com>
2024-07-10 02:05:25 +0530

    Update GetWON.ps1

--- GetWON.ps1
+++ GetWON.ps1
@@ -8,10 +8,7 @@ $platformToolsZip = Join-Path $wonDeployerDir "platform-tools-latest-windows.zip
 Invoke-WebRequest -Uri $platformToolsUrl -OutFile $platformToolsZip
 Expand-Archive -Path $platformToolsZip -DestinationPath $env:USERPROFILE -Force
 Remove-Item -Path $platformToolsZip -Force
-
 $platformToolsDir = Join-Path $env:USERPROFILE "platform-tools"
-
-# Update to download won_deployer.exe directly
 $wonDeployerExe = Join-Path $wonDeployerDir "won-deployer.exe"
 $wonDeployerUrl = "https://raw.githubusercontent.com/arkt-7/won-deployer/main/won_deployer.exe"
 Invoke-WebRequest -Uri $wonDeployerUrl -OutFile $wonDeployerExe
@@ -21,6 +18,7 @@ if ($currentPath -notcontains $wonDeployerDir) {
     [Environment]::SetEnvironmentVariable("PATH", "$wonDeployerDir;$($env:PATH)", "User")
     $env:PATH="$wonDeployerDir;$($env:PATH)"
     Write-Host "WoN-Deployer added to PATH. Restart shell to apply changes"
+    Write-Host "type  won-deployer  to run"
 }
 
 $currentPath = [Environment]::GetEnvironmentVariable("PATH", "User") -split ";"
