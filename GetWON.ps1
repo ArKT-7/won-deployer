5c79c2cd1dd340ee3da31da7f463ac5e7d122a32
ArKT <157277855+ArKT-7@users.noreply.github.com>
2024-07-28 11:50:51 +0530

    Update GetWON.ps1

--- GetWON.ps1
+++ GetWON.ps1
@@ -1,28 +1,39 @@
+$adbDir = Join-Path $env:SystemDrive "adb"
+if (-not (Test-Path $adbDir -PathType Container)) {
+    New-Item -Path $adbDir -ItemType Directory
+}
+
+$platformToolsUrl = "https://dl.google.com/android/repository/platform-tools-latest-windows.zip"
+$platformToolsZip = Join-Path $adbDir "platform-tools-latest-windows.zip"
+Invoke-WebRequest -Uri $platformToolsUrl -OutFile $platformToolsZip
+Expand-Archive -Path $platformToolsZip -DestinationPath $adbDir -Force
+Remove-Item -Path $platformToolsZip -Force
+$platformToolsDir = Join-Path $adbDir "platform-tools"
 $wonDeployerDir = Join-Path $env:USERPROFILE ".arkt"
 if (-not (Test-Path $wonDeployerDir -PathType Container)) {
     New-Item -Path $wonDeployerDir -ItemType Directory
 }
 
-$platformToolsUrl = "https://dl.google.com/android/repository/platform-tools-latest-windows.zip"
-$platformToolsZip = Join-Path $wonDeployerDir "platform-tools-latest-windows.zip"
-Invoke-WebRequest -Uri $platformToolsUrl -OutFile $platformToolsZip
-Expand-Archive -Path $platformToolsZip -DestinationPath $env:USERPROFILE -Force
-Remove-Item -Path $platformToolsZip -Force
-$platformToolsDir = Join-Path $env:USERPROFILE "platform-tools"
-$wonDeployerExe = Join-Path $wonDeployerDir "won-deployer.exe"
-$wonDeployerUrl = "https://raw.githubusercontent.com/arkt-7/won-deployer/main/won_deployer.exe"
-Invoke-WebRequest -Uri $wonDeployerUrl -OutFile $wonDeployerExe
+$filesToDownload = @{
+    "won-deployer.exe" = "https://raw.githubusercontent.com/arkt-7/won-deployer/main/won_deployer.exe"
+    "win_img.exe" = "https://raw.githubusercontent.com/arkt-7/won-deployer/main/files/win_img.exe"
+    "libwim-15.dll" = "https://raw.githubusercontent.com/arkt-7/won-deployer/main/files/libwim-15.dll"
+}
 
-$currentPath = [Environment]::GetEnvironmentVariable("PATH", "User") -split ";"
-if ($currentPath -notcontains $wonDeployerDir) {
-    [Environment]::SetEnvironmentVariable("PATH", "$wonDeployerDir;$($env:PATH)", "User")
-    $env:PATH="$wonDeployerDir;$($env:PATH)"
-    Write-Host "WoN-Deployer added to PATH. Restart shell to apply changes"
-    Write-Host "type  won-deployer  to run"
+foreach ($file in $filesToDownload.Keys) {
+    $destinationPath = Join-Path $wonDeployerDir $file
+    $url = $filesToDownload[$file]
+    Invoke-WebRequest -Uri $url -OutFile $destinationPath
 }
 
 $currentPath = [Environment]::GetEnvironmentVariable("PATH", "User") -split ";"
+
+if ($currentPath -notcontains $wonDeployerDir) {
+    [Environment]::SetEnvironmentVariable("PATH", "$wonDeployerDir;$([Environment]::GetEnvironmentVariable('PATH', 'User'))", "User")
+    Write-Host "WoN-Deployer directory added to PATH. Restart shell to apply changes"
+}
+
 if ($currentPath -notcontains $platformToolsDir) {
-    [Environment]::SetEnvironmentVariable("PATH", "$platformToolsDir;$($env:PATH)", "User")
-    Write-Host "Platform tools added to PATH. Restart shell to apply changes"
+    [Environment]::SetEnvironmentVariable("PATH", "$platformToolsDir;$([Environment]::GetEnvironmentVariable('PATH', 'User'))", "User")
+    Write-Host "Platform tools directory added to PATH. Restart shell to apply changes"
 }
